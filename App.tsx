bc4308a60414ebe3ac781a093467fa9759df3962
Raul Garcia <rraulg5@gmail.com>
2021-07-19 14:45:31 -0500

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { Text } from 'react-native';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Text>Navigation App</Text>
+    </NavigationContainer>
+  );
+};
+
+export default App;
